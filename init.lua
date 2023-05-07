fe75f88097653e76c27a4b7e1759056b2fa81ba7
Varun Balaji <varun@lightbeam.ai>
2023-05-07 17:44:20 +0530

    initial commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("config.lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy_setup")
---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,13 @@
+return
+  -- Kanagawa <3
+  {
+    "rebelot/kanagawa.nvim",
+    -- Added to copy LazyVim
+    -- lazy = true,
+    -- name = "kanagawa",
+    -- Commented
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  }
